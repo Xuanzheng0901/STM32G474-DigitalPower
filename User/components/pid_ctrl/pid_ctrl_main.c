ee948cbd85528ceb7819802f7d675b8ee9da242f
Xuanzheng0901 <szx1691196289@gmail.com>
2026-03-24 12:49:09 +0800

    perf(routine): 优化PID流程 1. 解耦ADC滤波 2. 移植mppt分支的控制adc采样逻辑调整

--- User/components/pid_ctrl/pid_ctrl_main.c
+++ User/components/pid_ctrl/pid_ctrl_main.c
@@ -19,37 +19,6 @@ static float now_current_A = 0.0f, now_voltage_mV = 0.0f;
 #define RC_ALPHA_DENO_2   0.10f
 #define RC_ALPHA_DENO_3   0.05f
 
-// float adc_batch_lowpass_filter(uint32_t *adc_raw_buf, uint8_t flag)
-// {
-//     float sum_filtered = 0.0f;  // 存储滤波后数据总和（避免溢出）
-//     static int32_t adc_filtered_buf[ADC_BUFFER_LENGTH / 2];
-//     // 1. 初始化滤波第一个值（原始值）
-//     adc_filtered_buf[0] = flag ? adc_raw_buf[0] & 0x0000FFFF : adc_raw_buf[0] >> 16;
-//     // 2. 对20个数据逐次做一阶RC低通滤波
-//     for(uint8_t i = 1; i < ADC_BUFFER_LENGTH / 2; i++)
-//     {
-//         // 核心滤波公式（整数运算：等效 filtered = α*last + (1-α)*raw）
-//         // 放大100倍避免小数，最后除以100，保证精度
-//         adc_filtered_buf[i] = (uint32_t)(
-//             FILTER_ALPHA * (float)adc_filtered_buf[i - 1] + (1.0f - FILTER_ALPHA) * flag
-//                 ? (float)(adc_raw_buf[0] & 0x0000FFFF)
-//                 : (float)(adc_raw_buf[i] >> 16));
-//
-//         // 边界保护：防止ADC值异常
-//         if(adc_filtered_buf[i] > 4095)
-//             adc_filtered_buf[i] = 4095;
-//         if(adc_filtered_buf[i] < 0)
-//             adc_filtered_buf[i] = 0;
-//     }
-//
-//     // 3. 计算滤波后20个数据的均值（最终输出，进一步平滑）
-//     for(uint8_t i = 0; i < ADC_BUFFER_LENGTH / 2; i++)
-//     {
-//         sum_filtered += (float)adc_filtered_buf[i];
-//     }
-//     return (sum_filtered / (float)(ADC_BUFFER_LENGTH / 2));
-// }
-
 static uint32_t voltage_to_duty(float expect_mV)
 {
     if(expect_mV < 10.0f)
@@ -69,9 +38,17 @@ static uint32_t voltage_to_duty(float expect_mV)
     return (uint32_t)(duty_cycle * PWM_PERIOD_ARR);
 }
 
-void pwm_set_duty(uint32_t pwm_duty)
+void pwm_set_duty(uint32_t pwm_duty) //占空比0-54400
 {
-    __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_3, (pwm_duty >> 1) + 1);
+    if(pwm_duty <= 27200)
+    {
+        __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_3, 27200 - (pwm_duty >> 1) + 1);
+    }
+    else
+    {
+        __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_3, (pwm_duty >> 1) + 1);
+    }
+
     hhrtim1.Instance->sTimerxRegs[TIMER_E].CMP1CxR = pwm_duty;
 }
 
@@ -85,6 +62,40 @@ float get_voltage_value(uint8_t index)
     return 0.0f;
 }
 
+static float last_last_voltage_mV = 0.0f;
+static float last_last_current_A = 0.0f;
+
+// 把DMA块数据转换为电压/电流工程量，并做电压去噪
+static inline void adc_data_process(uint32_t *data_buf)
+{
+    float origin_voltage_sum = 0.0f, origin_current_sum = 0.0f;
+    for(uint8_t i = 0; i < ADC_BUFFER_LENGTH / 2; i++)
+    {
+        origin_voltage_sum += data_buf[i] & 0x0FFF;
+        origin_current_sum += data_buf[i] >> 16;
+    }
+    // origin_voltage_sum / 4095 * 3300 / 20(样本量) = adc引脚上的电压
+    // * 20 = 运放输入电压(运放20分压)
+    // * 1.0079(adc误差修正)
+    float temp_voltage_mV = origin_voltage_sum * 3300.0f / 4095.0f / (float)(ADC_BUFFER_LENGTH / 2) * 20.0f *
+                            1.0079f;
+
+    float temp_current_A = origin_current_sum * 3300.0f / 4095.0f / (ADC_BUFFER_LENGTH / 2) * 2.0f / 1050.0f; //单位mA
+
+    //对均值进行二阶rc滤波
+    temp_voltage_mV = RC_ALPHA_DENO_1 * temp_voltage_mV + RC_ALPHA_DENO_2 * now_voltage_mV +
+                      RC_ALPHA_DENO_3 * last_last_voltage_mV;
+
+    temp_current_A = RC_ALPHA_DENO_1 * temp_current_A + RC_ALPHA_DENO_2 * now_current_A +
+                     RC_ALPHA_DENO_3 * last_last_current_A;
+
+    last_last_voltage_mV = now_voltage_mV;
+    last_last_current_A = now_current_A;
+
+    now_voltage_mV = temp_voltage_mV;
+    now_current_A = temp_current_A;
+}
+
 
 static void PID_ctrl_routine(void *pvParameters)
 {
@@ -94,10 +105,6 @@ static void PID_ctrl_routine(void *pvParameters)
     static float next_output_voltage_mV = 0.0f;
 
     static uint32_t *buf_ptr;
-
-    static float temp_current_voltage_mV = 0.0f;
-    static float last_voltage_mV = 0.0f;
-    static float last_last_voltage_mV = 0.0f;
     while(1)
     {
         //1. 等待ADC数据
@@ -112,43 +119,15 @@ static void PID_ctrl_routine(void *pvParameters)
                     // pid_reset_ctrl_block(pid_handle); //使用增量式pid更改target后不能重置
                 }
             }
-
-            //3. 计算实际电压/电流
-            // float origin_voltage_sum = adc_batch_lowpass_filter(buf_ptr, 1);
-            // float origin_current_sum = adc_batch_lowpass_filter(buf_ptr, 0);
-            float origin_voltage_sum = 0.0f, origin_current_sum = 0.0f;
-            for(uint8_t i = 0; i < ADC_BUFFER_LENGTH / 2; i++)
-            {
-                origin_voltage_sum += buf_ptr[i] & 0x0FFF;
-                origin_current_sum += buf_ptr[i] >> 16;
-            }
-            // 读取到的电压  20分压
-            temp_current_voltage_mV = origin_voltage_sum / 4095.0f * 3300.0f * 20.0f / (float)(ADC_BUFFER_LENGTH / 2)
-                                      * 1.0048f;
-            if(temp_current_voltage_mV >= 10.0f)
-                temp_current_voltage_mV += 60.0f;
-            if(temp_current_voltage_mV < 0)
-                temp_current_voltage_mV = 0;
-
-            now_current_A = origin_current_sum / 4095.0f * 3300.0f * 2.0f / 1000.0f / (
-                                ADC_BUFFER_LENGTH / 2); //单位A
-
-            //4. 对均值进行二阶rc滤波
-            temp_current_voltage_mV = RC_ALPHA_DENO_1 * temp_current_voltage_mV + RC_ALPHA_DENO_2 * last_voltage_mV +
-                                      RC_ALPHA_DENO_3 * last_last_voltage_mV;
-            now_voltage_mV = temp_current_voltage_mV;
-            last_last_voltage_mV = last_voltage_mV;
-            last_voltage_mV = temp_current_voltage_mV;
-
+            adc_data_process(buf_ptr);
 
             //4. 进行pid计算
-            float error_mV = (float)target_voltage_mV - temp_current_voltage_mV;
+            float error_mV = (float)target_voltage_mV - now_voltage_mV;
 
             pid_compute(pid_handle, error_mV, &next_output_voltage_mV);
             uint32_t output_duty = voltage_to_duty(next_output_voltage_mV);
             pwm_set_duty(output_duty);
         }
-        // vTaskDelay(1);
     }
 }
 
