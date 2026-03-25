0c73f4ec2a6fdcd867c8599df64ad59dc0e40f00
Xuanzheng0901 <szx1691196289@gmail.com>
2026-03-25 23:53:24 +0800

    feat(displayDriver): 增加显示驱动抽象层, 允许选择驱动ic

---
+++ User/components/display/display.h
@@ -0,0 +1,33 @@
+#ifndef G474_1_DISPLAY_H
+#define G474_1_DISPLAY_H
+
+#include "stdio.h"
+#include "main.h"
+
+#define DISPLAY_SH1107   0x01
+#define DISPLAY_SSD1306  0x02
+
+#define DISPLAY_USE DISPLAY_SH1107
+
+#if DISPLAY_USE == DISPLAY_SSD1306
+#include "ssd1306.h"
+#define DISP_HOR_RES    128
+#define DISP_VER_RES    64
+#define DISP_BUF_SIZE   (DISP_HOR_RES * DISP_VER_RES / 8 + 8)
+#define DISPLAY_INIT()  ssd1306_init()
+#define DISPLAY_DRAW(buffer) ssd1306_draw(buffer)
+#elif DISPLAY_USE == DISPLAY_SH1107
+#include "sh1107.h"
+#define DISP_HOR_RES    128
+#define DISP_VER_RES    128
+#define DISP_BUF_SIZE   (DISP_HOR_RES * DISP_VER_RES / 8 + 8)
+#define DISPLAY_INIT()  sh1107_init()
+#define DISPLAY_DRAW(buffer) sh1107_draw(buffer)
+#else
+#error "Unsupported DISPLAY_USE value"
+#endif
+
+#define display_init()  DISPLAY_INIT()
+#define display_draw(buffer) DISPLAY_DRAW(buffer)
+
+#endif //G474_1_DISPLAY_H
