954355dcb338b4cb4e189527a4b4d7c9d50f0edc
Xuanzheng0901 <szx1691196289@gmail.com>
2026-03-27 22:28:21 +0800

    feat(stdio): 重写stdio实现
    使用性能更高重写_write()的整帧发送代替_ioputchar()的单字节发送

--- User/main/IO.c
+++ User/main/IO.c
@@ -1,13 +1,13 @@
 #include "usart.h"
 #include "string.h"
 
-#ifdef __GNUC__
-#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif
-PUTCHAR_PROTOTYPE
+#include <unistd.h> // 必须包含此头文件
+
+// 重写底层 _write 函数
+int _write(int f, char *p, int l)
 {
-    HAL_UART_Transmit(&huart3, (uint8_t *)&ch, 1, 0xFFFF);
-    return ch;
+    // ptr 是缓冲区首地址，len 是本次要打印的字符串长度
+    // 使用阻塞式发送，但因为是按块发送，效率远高于逐字节发送
+    HAL_UART_Transmit(&huart3, (uint8_t *)p, l, HAL_MAX_DELAY);
+    return l;
 }
